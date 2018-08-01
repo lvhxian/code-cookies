1b9370127ac66207a9b36c70604e22850f419f01
iTobys <1378993548@qq.com>
2018-08-01 17:38:36 +0800

    codeCookies配置项

--- config/rollup.config.prod.js
+++ config/rollup.config.prod.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
-    input: 'src/EasyCookies.js',
+    input: 'src/CodeCookies.js',
     output: {
         file: 'dist/bundle.js',
         format: 'cjs'
